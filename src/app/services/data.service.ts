ce789b63abc8f559b4d5e89e2f6dd1a190082008
BachuShravani <shravani17695@gmail.com>
2023-10-13 15:06:07 +0100

    dataservice

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -3,53 +3,52 @@ import { NotFoundError } from './../common/not-found-error';
 import { AppError } from './../common/app-error';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/toPromise';
-import 'rxjs/add/observable/throw';
+import { Observable, catchError, map, throwError } from 'rxjs';
 
-@Injectable()
 export class DataService {
-  constructor(private url: string, private http: HttpClient) { }
+  constructor(private url: string, private http: HttpClient) {}
 
   getAll() {
-    return this.http.get(this.url)
-      .map((response:any) => response.json())
-      .catch(this.handleError);
+    return this.http
+      .get(this.url)
+      .pipe(map((response: any) => response.json()))
+      .pipe(catchError(this.handleError));
   }
 
-  get(id:any) {
-    return this.http.get(this.url + '/' + id)
-      .map((response:any) => response.json())
-      .catch(this.handleError);
+  get(id: any) {
+    return this.http
+      .get(this.url + '/' + id)
+      .pipe(map((response: any) => response.json()))
+      .pipe(catchError(this.handleError));
   }
 
-  create(resource:any) {
-    return this.http.post(this.url, JSON.stringify(resource))
-      .map((response:any) => response.json())
-      .catch(this.handleError);
+  create(resource: any) {
+    return this.http
+      .post(this.url, JSON.stringify(resource))
+      .pipe(map((response: any) => response.json()))
+      .pipe(catchError(this.handleError));
   }
 
-  update(resource:any) {
-    return this.http.patch(this.url + '/' + resource.id, JSON.stringify({ isRead: true }))
-      .map((response:any) => response.json())
-      .catch(this.handleError);
+  update(resource: any) {
+    return this.http
+      .patch(this.url + '/' + resource.id, JSON.stringify({ isRead: true }))
+      .pipe(map((response: any) => response.json()))
+      .pipe(catchError(this.handleError));
   }
 
-  delete(id:any) {
-    return this.http.delete(this.url + '/' + id)
-      .map((response:any) => response.json())
-      .toPromise()
-      .catch(this.handleError);
+  delete(id: any) {
+    return this.http
+      .delete(this.url + '/' + id)
+      .pipe(map((response: any) => response.json()))
+      .pipe(catchError(this.handleError));
   }
 
   private handleError(error: Response) {
     if (error.status === 400)
-      return Observable.throw(new BadInput(error.json()));
+      return Observable
+      .throw(new BadInput(error.json()));
 
-    if (error.status === 404)
-      return Observable.throw(new NotFoundError());
+    if (error.status === 404) return Observable.throw(new NotFoundError());
 
     return Observable.throw(new AppError(error));
   }
